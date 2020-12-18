c67fdc8464cdb1a0e4823e33cddf6e32f5c7cf1d
Syndicate555 <saziz028@uottawa.ca>
2020-12-17 20:22:02 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const dotenv = require('dotenv')
-const connectDB = require('./config/db')
+const connectDB = require('./config/db') // connecting the MongoDB database
+const morgan = require('morgan') // for log in
 //load config 
 dotenv.config({path: './config/config.env'})
 
 connectDB()
 const app = express()
+
+if (process.env.NODE_ENV === 'development') {
+ app.use(morgan('dev'))
+}
 const PORT = process.env.PORT || 4545
 app.listen(PORT, console.log(`Surver running in ${process.env.NODE_ENV} mode on port ${PORT}`))
