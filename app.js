bbd4da6d6ef025e083c07bd853e2e440ca7da448
Syndicate555 <saziz028@uottawa.ca>
2020-12-17 19:55:24 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,4 +5,4 @@ const dotenv = require('dotenv')
 dotenv.config({path: './config/config.env'})
 const app = express()
 const PORT = process.env.PORT || 4545
-app.listen(PORT, console.log(`Surver running in PORT ${PORT}`))
+app.listen(PORT, console.log(`Surver running in ${process.env.NODE_ENV} mode on port ${PORT}`))
