995a3861016c54176b94cad69b9b5521ec2f0594
Syndicate555 <saziz028@uottawa.ca>
2020-12-17 21:08:45 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,15 @@ const express = require('express')
 const router = express.Router()
 
 
+// GET request Login/Landing Page
+router.get('/', (req, res) => {
+ res.send('Login')
+})
 
+
+router.get('/', (req, res) => {
+ res.send('Dashboard')
+})
 
 
 module.exports = router
