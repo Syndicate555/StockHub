702bf08c29ae63dab1b2d7c598c1811f16b4ff50
Syndicate555 <saziz028@uottawa.ca>
2020-12-19 02:44:47 -0500

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@
 
 const express = require('express')
 const router = express.Router()
-
+const passport = require('passport')
 
 // GET request Authenticate with google
 router.get('/google', passport.authenticate('google', { scope:['profile']}))
