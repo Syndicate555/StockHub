9151c4a018a684da05934b44389ac0d5d058d7e5
Syndicate555 <saziz028@uottawa.ca>
2020-12-21 02:04:26 -0500

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -14,4 +14,11 @@ router.get('/google/callback', passport.authenticate('google', { failureRedirect
  res.redirect('/dashboard')
 })
 
+//@desc Logout User 
+
+router.get('/logout', (req, res) => {
+ req.logout()
+ res.redirect('/')
+})
+
 module.exports = router
