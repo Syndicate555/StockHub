3a0d372f952cca8f855f1ea0eb062ae929c773c1
Saffat Aziz <saziz028@uottawa.ca>
2020-12-26 20:02:43 +0600

    Update stories.js

--- routes/stories.js
+++ routes/stories.js
@@ -3,7 +3,8 @@ const router = express.Router()
 const {ensureAuth} = require('../middlewares/auth') // destructuring
 const Story  = require('../models/Story')
 
-// GET request Login/Landing Page
+// @desc  Show add page
+// @route GET /stories/add
 router.get('/', ensureGuest, (req, res) => { // middleware applied
  res.render('login', {
   layout:'login'
