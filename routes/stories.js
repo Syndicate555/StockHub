8588a4885d12b63af35db63992149f7bcb660a27
Saffat Aziz <saziz028@uottawa.ca>
2020-12-26 20:01:29 +0600

    Create stories.js

---
+++ routes/stories.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+const {ensureAuth, ensureGuest} = require('../middlewares/auth') // destructuring
+const Story  = require('../models/Story')
+
+// GET request Login/Landing Page
+router.get('/', ensureGuest, (req, res) => { // middleware applied
+ res.render('login', {
+  layout:'login'
+ })
+})
+
+
+
+
+module.exports = router
