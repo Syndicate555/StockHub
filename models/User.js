80817cacfe6030186266a2908ec6ee9a2a9d175e
Saffat Aziz <saziz028@uottawa.ca>
2021-01-01 21:27:30 +0600

    s

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const UserSchema = new mongoose.Schema({
  googleId: {
   type: String,
-  required: true
+  // required: true
  },
  displayName: {
   type: String,
