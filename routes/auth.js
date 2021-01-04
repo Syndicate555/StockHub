7ad4bcd6b80c443ab2d74d2f238433ef84c61905
Saffat Aziz <saziz028@uottawa.ca>
2021-01-04 18:45:02 +0600

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,22 @@ const passport = require('passport')
 // GET request Authenticate with google
 router.get('/google', passport.authenticate('google', { scope:['profile']}))
 
+router.get('/',  async (req, res) => { // middleware applied
+    try {
+        const email = req.body.email;
+        const password = req.body.password;
+        const useremail = await Register.findOne({email:email})
+        if (useremail.password === password){
+            res.redirect('/dashboard')
+         // res.redirect('/dashboard')
+        } else{
+            alert("invalid password")
+        }
+    } catch (error) {
+        console.error(error)
+        alert("Invalid email")
+    }
+    })
 
 // Google auth callback 
 // GET request
