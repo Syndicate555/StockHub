76c8cc0d848a7555e10850b896f981492835c8f2
Saffat Aziz <saziz028@uottawa.ca>
2021-01-05 23:38:44 +0600

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,7 @@
 const GoogleStrategy = require('passport-google-oauth20').Strategy
 const mongoose = require('mongoose')
 const User = require('../models/User')
-const LocalStrategy = require('passport-local')
+const LocalStrategy = require('passport-local').Strategy
 module.exports = function (passport) {
  passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
