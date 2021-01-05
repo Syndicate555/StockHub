10729ee3e7b022fa29db9d9b45574250d235d366
Saffat Aziz <saziz028@uottawa.ca>
2021-01-05 23:23:00 +0600

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -2,16 +2,3 @@ const express = require("express")
 const router = express.Router()
 const {ensureAuth, ensureGuest} = require('../middlewares/auth') // destructuring
 
-
-// Login Page
-router.get('/', ensureGuest, (req, res) => { // middleware applied
-    res.render('login', {
-     layout:'login'
-    })
-   })
-
-router.get('/register', ensureGuest, (req, res) => { // middleware applied
-    res.render('register', {
-     layout:'register'
-    })
-})
