07efc8194afb81e36969e8160d9dd241b15b1421
Saffat Aziz <saziz028@uottawa.ca>
2021-01-06 01:27:02 +0600

    Update passport1.js

--- config/passport1.js
+++ config/passport1.js
@@ -4,3 +4,26 @@ const User = require('../models/User') // for google login
 const LocalStrategy = require('passport-local').Strategy
 const bcrypt = require('bcryptjs')
 const Register = require('../models/Registers') 
+
+module.exports = function(passport){
+    passport.use(
+      new LocalStrategy({
+        usernameField: 'email'
+      }, (email, password, done) => {
+        // Match User
+        User.findOne({ email: email}).then(user => {
+          if (!user) {
+            return done(null, false, {message: 'That email is not registered'})
+          }
+          bcrypt.compare(password, user.password, (err, isMatch) => {
+            if (err) throw err;
+            if (isMatch){
+              return done(null, user);
+            } else{
+              return done(null, false, {message: 'Password incorrect'})
+            }
+          })
+        })
+      })
+    )
+  }
