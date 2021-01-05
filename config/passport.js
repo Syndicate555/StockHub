74cd0a2a9b29e1e12906b12dedf0b06d0a85e7c1
Saffat Aziz <saziz028@uottawa.ca>
2021-01-06 01:29:28 +0600

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -5,29 +5,28 @@ const LocalStrategy = require('passport-local').Strategy
 const bcrypt = require('bcryptjs')
 const Register = require('../models/Registers') // for email login
 
-function myfunction(passport){
-  passport.use(
-    new LocalStrategy({
-      usernameField: 'email'
-    }, (email, password, done) => {
-      // Match User
-      User.findOne({ email: email}).then(user => {
-        if (!user) {
-          return done(null, false, {message: 'That email is not registered'})
-        }
-        bcrypt.compare(password, user.password, (err, isMatch) => {
-          if (err) throw err;
-          if (isMatch){
-            return done(null, user);
-          } else{
-            return done(null, false, {message: 'Password incorrect'})
-          }
-        })
-      })
-
-    })
-  )
-}
+// function myfunction(passport){
+//   passport.use(
+//     new LocalStrategy({
+//       usernameField: 'email'
+//     }, (email, password, done) => {
+//       // Match User
+//       User.findOne({ email: email}).then(user => {
+//         if (!user) {
+//           return done(null, false, {message: 'That email is not registered'})
+//         }
+//         bcrypt.compare(password, user.password, (err, isMatch) => {
+//           if (err) throw err;
+//           if (isMatch){
+//             return done(null, user);
+//           } else{
+//             return done(null, false, {message: 'Password incorrect'})
+//           }
+//         })
+//       })
+//     })
+//   )
+// }
 
 
 function myfunction1(passport) {
