73a5204d4188c73fee5a1c935a6151ff62c42883
Saffat Aziz <saziz028@uottawa.ca>
2021-01-10 18:11:22 +0600

    e

--- app.js
+++ app.js
@@ -8,13 +8,14 @@ const path = require('path')
 const morgan = require('morgan')
 const myFunctions = require('./config/passport')
 const app = express();
+const dotenv = require('dotenv')
 require('./config/passport1')(passport)
 // Passport Config
 myFunctions.myFunction1(passport)
 // myFunctions.myFunction2(passport)
 // DB Config
 const db = require('./config/keys').mongoURI;
-
+dotenv.config({path:'./config/config.env'})
 
 
 // static folder
@@ -67,6 +68,6 @@ app.use('/users', require('./routes/users.js'));
 app.use('/stories', require('./routes/stories.js'));
 app.use('/auth', require('./routes/auth.js'))
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running on  ${PORT}`));
+app.listen(PORT, console.log(`Server running on  port ${PORT}`));
