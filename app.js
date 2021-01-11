9cc73abb2d6454a749bbfb93287659b7b36f7cec
Saffat Aziz <saziz028@uottawa.ca>
2021-01-12 02:42:36 +0600

    Update app.js

--- app.js
+++ app.js
@@ -103,4 +103,7 @@ app.use('/auth', require('./routes/auth.js'))
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running on  port ${PORT}`));
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
