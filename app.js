edd6cdc32e8b5dea6d3dded127e1dae438df006f
Saffat Aziz <saziz028@uottawa.ca>
2021-01-12 02:49:55 +0600

    Update app.js

--- app.js
+++ app.js
@@ -25,13 +25,13 @@ app.use(express.static(path.join(__dirname, 'public')))
 // Connect to MongoDB
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const connection = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
     })
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`)
+    console.log(`MongoDB Connected: ${connection.connection.host}`)
   } catch (err) {
     console.error(err)
     process.exit(1)
