b90b1bbcbc2812d89e91b893e961bd8039a15afb
Saffat Aziz <saziz028@uottawa.ca>
2021-01-12 15:45:30 +0600

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -10,6 +10,7 @@ router.get('/google', passport.authenticate('google', { scope:['profile']}))
 // Google auth callback 
 // GET request
 router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/' }), (req, res) => {
+
  res.redirect('/dashboard')
 })
 
