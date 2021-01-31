b4645271a07ceef892af4671a49788f5255bafac
Syndicate555 <saziz028@uottawa.ca>
2021-01-30 22:27:25 -0500

    some changes

--- routes/chat.js
+++ routes/chat.js
@@ -1,15 +1,13 @@
 const express = require('express');
-const router = express.Router();
+const router = express.Router()
 const bcrypt = require('bcryptjs');
 const passport = require('passport');
 const alert = require('alert');
 const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
 
-router.get('/chat', ensureGuest, (req, res) => {
-  res.render('chat', {
-    layout: 'login',
-  })
-})
+router.get('/chatrooms', ensureAuth, (req, res) =>res.render('chat', {
+  layout:'login'
+}));
 
 module.exports = router
