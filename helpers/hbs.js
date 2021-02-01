9a1c2c003019674a37425f2207438970404ee806
Syndicate555 <saziz028@uottawa.ca>
2021-02-01 04:25:36 -0500

    changes

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -23,14 +23,12 @@ module.exports = {
   //     '$& selected="selected"');
     
   // },
-  elect: function (value, options) {
-    return options.fn(this)
-    .split('\n')
-    .map(function(v) {
-      var t = 'value="' + value + '"'
-      return ! RegExp(t).test(v) ? v : v.replace(t, t + ' selected="selected"')
-    })
-    .join('\n')
+  elect: function (state, value) {
+    if (state === value) {
+      return 'selected';
+    }
+    return'';
+    
    
   },
   
